f9fb686980495acae5d1f07b607f96ab03f0acef
kathrynzaro <zarokathryn@gmail.com>
2022-05-02 12:35:50 -0700

    remove event listener

--- app.js
+++ app.js
@@ -52,7 +52,8 @@ form.addEventListener('submit', (e) => {
 });
 
 remove.addEventListener('click', () => {
-    
+    stats.pop();
+    renderStats();
     // Step 2 -- add code to allow users to remove the most recent stat
     // Hint -- how do you remove an element from an array?
     // Hint -- how can we rerender the stats using a function above?
